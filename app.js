70907d1820181b18ff79529375f5f806f761579b
pranitaankalikar <82073936+pranitaankalikar@users.noreply.github.com>
2021-06-16 18:29:05 +0530

    Retrieving DataFromDatabases & deletefunctionality

--- app.js
+++ app.js
@@ -14,7 +14,14 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-db.execute('SELECT*FROM products');
+db.execute('SELECT*FROM products')
+.then(result=>{
+console.log(result);
+})
+.catch(error=>{
+    console.log("pranita")
+    console.log(error);
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
--- util/database.js
+++ util/database.js
@@ -1,9 +1,10 @@
 const mysql=require('mysql2');
 const pool=mysql.createPool({
-    host='localhost',
-    user='root',
-    database='node-complete',
-    password='pranita@1998',
-
+    host:'localhost',
+    user:'root',
+    database:'node_complete',
+    password:'password',
+    port:'3306'
 });
+
 module.exports=pool.promise();
