ddf8434bb27cac5626611fdc3540d21f38ff8ba4
pranitaankalikar <82073936+pranitaankalikar@users.noreply.github.com>
2021-06-17 14:09:06 +0530

    Design The User And Product Relationship

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+
+
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product=require('./models/product');
+const User=require('./models/user');
 
 const app = express();
 
@@ -17,15 +21,40 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req,res,next)=>{
+  User.findAll({ where: { id: 1 } })
+     .then(users=>{
+       req.user=users[0];
+       next();
+     })
+     .catch(err => console.log(err));
+
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User,{constraints:true,onDelete:'CASCADE'});
+User.hasMany(Product);
+
 sequelize
+  //.sync({force:true})
   .sync()
-  .then(result => {
+  .then(result=>{
+    return User.findAll({ where: { id: 1 } });
+    
     // console.log(result);
+  })
+  .then(user=>{
+    if(!user[0]){
+      return User.create({name:'Pranita',email:'pranita@gmail.com'});
+    }
+    return user[0];
+  })
+  .then(user => {
+    //console.log(user);
     app.listen(5000);
   })
   .catch(err => {
--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,8 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user
+  .createProduct({
     title: title,
     price: price,
     imageUrl: imageUrl,
@@ -35,7 +36,9 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findAll({ where: { id: prodId } })
+  req.user
+  .getProducts({where:{id:prodId}})
+  //Product.findAll({ where: { id: prodId } })
      .then(products => {
       if (!products[0]) {
         return res.redirect('/');
@@ -87,7 +90,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  //Product.findAll()
+  req.user.getProducts()
     .then(products => {
       res.render('admin/products', {
         prods: products,
